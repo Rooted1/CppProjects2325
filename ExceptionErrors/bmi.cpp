c30a00589b17197c927732cd2b6a1424e3582156
Ruth Obe <ruthobey@gmail.com>
2022-09-06 18:54:26 -0500

    use if/else contitionals to check for errors

--- ExceptionErrors/bmi.cpp
+++ ExceptionErrors/bmi.cpp
@@ -17,16 +17,31 @@ int main () {
         // Get user data
         cout << "Enter weight (in pounds): ";
         cin >> weightVal;
-        cout << "Enter height (in inches): ";
-        cin >> heightVal;
 
+        // Error checking for non-negative weight
+        if (weightVal < 0) {
+            cout << "Invalid weight." << endl;
+        }
+        else {
+            cout << "Enter height (in inches): ";
+            cin >> heightVal;
+
+            // Error checking for non-negative height
+            if (heightVal < 0){
+                cout <<  "Invalid height" << endl;
+            }
+        }
 
         // calculate BMI value
-        bmiCalc = (static_cast<float>(weightVal) / static_cast<float>(heightVal * heightVal)) * 703.0;
-
-        // Print user health info
-        cout << "BMI: " << bmiCalc << endl;
-        cout << "(CDC: 18.6-24.9 normal)" << endl;
+        if ((weightVal <= 0) || (heightVal <= 0)){
+            cout << "Cannot compute info." << endl;
+        }
+        else {
+            bmiCalc = (static_cast<float>(weightVal) / static_cast<float>(heightVal * heightVal)) * 703.0;
+            // Print user health info
+            cout << "BMI: " << bmiCalc << endl;
+            cout << "(CDC: 18.6-24.9 normal)" << endl;
+        }
 
         // Get user prompt to continue/quit
         cout << endl << "Enter any key ('q' to quit): ";
