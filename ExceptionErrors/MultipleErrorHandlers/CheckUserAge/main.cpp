4710c73a04756310b19411f75657294e80f8e9e1
Ruth Obe <ruthobey@gmail.com>
2022-09-12 22:12:26 -0500

    check if user entered string instead of integer using exception error handlers

---
+++ ExceptionErrors/MultipleErrorHandlers/CheckUserAge/main.cpp
@@ -0,0 +1,37 @@
+// This program takes single-word first names and ages
+// and outputs the name with the age incremented.
+// The program fails and throws an exception if the age input 
+// is a string rather than an int. 
+// Add a try/catch statement to catch ios_base::failure, and output 0 for the age
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string inputName;
+    int age;
+    
+    // Set exception mask for cin stream
+    cin.exceptions(ios::failbit);
+
+    cin >> inputName;
+    while (inputName != "-1") {
+        try {
+            cin >> age;
+            if (cin.fail()) {
+                throw ios_base::failure("z");
+            }
+
+            cout << inputName << " " << (age+1) << endl;
+        }
+        catch (ios_base::failure& excpt){
+            cout << inputName << " " << 0 << endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+        }
+        cin >> inputName;
+    }
+    return 0;
+}
