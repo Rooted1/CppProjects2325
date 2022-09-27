bdcc7da9e62851bb616b7cf09d1eabd2276909a4
Ruth Obe <ruthobey@gmail.com>
2022-09-27 15:42:59 -0500

    calculate user age in minutes and account for leap years

--- C++Variables/UserAgeHeartbeats/main.cpp
+++ C++Variables/UserAgeHeartbeats/main.cpp
@@ -8,13 +8,18 @@ using namespace std;
 int main(){
     int userAgeYears;
     int userAgeDays;
+    int userAgeMinutes;
 
     cout << "Enter your age in years: ";
     cin >> userAgeYears;
 
-    userAgeDays = userAgeYears * 365;
+    userAgeDays = userAgeYears * 365;               // calculate days without leap years
+    userAgeDays = userAgeDays + (userAgeYears / 4); // Add days for leap years
 
     cout << "You are " << userAgeDays << " days old." << endl;
+
+    userAgeMinutes = userAgeDays * 24 * 60;
+    cout << "You are " << userAgeMinutes << " minutes old." << endl;
 
     return 0;
 }
